04e20cd4ac5b832532543d7f7615e93c3f9597ed
mary kere <marykere20@gmail.com>
2024-11-20 11:56:17 +0300

    Updated: Tasks and Roles models and respective routes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from werkzeug import generate_password_hash
+from models import User, db, Task
 
 app = Flask(__name__)
 
@@ -15,11 +15,74 @@ def home():
 def about():
     return "<h1>About page!</h1>"
 
-@app.route('/register')
+@app.route('/register', methods =['POST'])
 def register():
     username = request.json['username']
     email = request.json['email']
     password = request.json['password']
-    hashed_password = generate_password_hash()
+    confirm_password = request.json['confirm_password']
+    
+    user = User(email=email, username=username)
+    user.set_password(password)
+    
+    if user.query.filter_by(
+        username=username).first() or User.query.filter_by(email=email):
+        return jsonify({"message":"Username or Email already exists"}), 400
+    
+    if not username or email or password:
+        return jsonify({"message":"All fields are required"}), 400
+    
+    if password!= confirm_password:
+        return jsonify({"message":"Passwords do not match"}), 400
+    
+    db.session.add(user)
+    db.session.commit(user)
+
+    return jsonify ({"message":"User registered successfully"}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.json['email']
+    password = request.json['password']
+    
+    user = User.query.filter_by(email=email).first()
+    
+    if not user or not user.check_password(password):
+        return jsonify({"message":"Invalid email or password"}), 401
+    
+    return jsonify({"message":"Logged in successfully"}), 200
+
+@app.route('/create_task', methods=['POST'])
+def create_task():
+    title = request.json['title']
+    description = request.json['description']
+    user_id = request.json['user_id']
+    
+    task = Task(title=title, description=description, user_id=user_id)
+    
+    db.session.add(task)
+    db.session.commit(task)
+    
+    return jsonify({"message":"Task created succesfully"}), 201
+
+@app.route('/task', methods=(['GET'], ['POST']))
+def create_and_retrieve_task():
+    if request.method==['GET']:
+        task = User.query.get('task_id')
+    if request.method==['POST']:
+        id = request.json['id']
+        title = request.json['title']
+        description = request.json['description']
+        user_id = request.json['user_id']
+        
+        task = Task(id=id, title=title, description=description, user_id=user_id)
+        
+        db.session.add(task)
+        db.session.commit(task)
+        
+        return jsonify({"message":"Task created succesfully"}), 201
+
+
+    
 
 
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 import app
-from werkzeug import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db=SQLAlchemy()
 
@@ -9,3 +9,35 @@ class User(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     username=db.Colum(db.String(80), unique=True, nullable=False)
     email=db.Column(db.String(120), unique=True, nullable=False)
+    password_hash= db.Column(db.String(256), nullable=False)
+    
+    def set_password (self, password):
+        self.password_hash = generate_password_hash(password)
+    def check_password (self, password):
+        return check_password_hash(self.password_hash, password)
+    task = db.relationship('tasks', backref='user', lazy=True)
+    role = db.relationship('roles', backref='user', lazy=True)
+    
+    def __refr__(self):
+        return f"User: {self.username}, Email: {self.email}"
+    
+class Task(db.Model):
+    __tablename__='tasks'
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(80), nullable=False)
+    description=db.Column(db.String(250), nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    role=db.relationship('Role', backref='task', lazy=True)
+    def __repr__(self):
+        return f"Task: {self.title}, Description: {self.description}, User: {self.user.username}"
+    
+class Role(db.Model):
+    __tablename__='roles'
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(80), unique=True, nullable=False)
+    description=db.Column(db.String(250), nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    task_id=db.Column(db.Integer, db.ForeignKey(Task.id), nullable=False)
+    
+    def __repr__(self):
+        return f"Role: {self.name}, Description: {self.description}, User: {self.user.username}, Task: {self.task.title}"
